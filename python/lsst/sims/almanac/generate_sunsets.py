f25540a5dc9bd740632468c6d46d7c21a5994d54
Peter Yoachim <yoachim@uw.edu>
2020-07-07 17:43:12 -0700

    fix missing nights

--- python/lsst/sims/almanac/generate_sunsets.py
+++ python/lsst/sims/almanac/generate_sunsets.py
@@ -8,11 +8,9 @@ from lsst.sims.utils import Site
 # Trying out the astroplan sunrise/set code.
 # conda install -c astropy astroplan
 mjd_start = 59853.5 - 3.*365.25
-duration = 25.*365.25
+duration = 177 #25.*365.25
 pad_around = 40
-t_step = 0.7
-
-mjds = np.arange(mjd_start-pad_around, duration+mjd_start+pad_around+t_step, t_step)
+t_step = 0.2
 
 site = Site('LSST')
 observer = Observer(longitude=site.longitude*u.deg, latitude=site.latitude*u.deg,
@@ -22,38 +20,35 @@ observer = Observer(longitude=site.longitude*u.deg, latitude=site.latitude*u.deg
 # This blows up if I try to do it all at once? 250 GB of memory?
 results = []
 
-mjds_list = np.array_split(mjds, 500)
+mjd = mjd_start + 0
+counter = 0
+names = ['night', 'sunset', 'sun_n12_setting', 'sun_n18_setting', 'sun_n18_rising',
+         'sun_n12_rising', 'sunrise', 'moonrise', 'moonset']
+types = [int]
+types.extend([float]*(len(names)-1))
+base_al = np.zeros(1, dtype=list(zip(names, types)))
 
-for i, mjds in enumerate(mjds_list):
-    print('chunk %i of %i'%(i, len(mjds_list)))
-    times = Time(mjds, format='mjd')
-    print('getting sunsets')
-    sunsets = observer.sun_set_time(times, which='nearest')
+while mjd < (mjd_start+duration):
+    times = Time(mjd, format='mjd')
+    sunsets = observer.sun_set_time(times, which='next')
+    times = sunsets
 
-    sunsets = np.unique(np.round(sunsets.mjd, decimals=4))
+    almanac = base_al.copy()
+    almanac['sunset'] = sunsets.mjd
 
-    names = ['night', 'sunset', 'sun_n12_setting', 'sun_n18_setting', 'sun_n18_rising',
-             'sun_n12_rising', 'sunrise', 'moonrise', 'moonset']
-    types = [int]
-    types.extend([float]*(len(names)-1))
-    almanac = np.zeros(sunsets.size, dtype=list(zip(names, types)))
-    almanac['sunset'] = sunsets
-
-    times = Time(sunsets, format='mjd')
-    print('evening twilight 1')
+    almanac['moonset'] = observer.moon_set_time(times, which='next').mjd
+    almanac['moonrise'] = observer.moon_rise_time(times, which='next').mjd
     almanac['sun_n12_setting'] = observer.twilight_evening_nautical(times, which='next').mjd
-    almanac['sun_n18_setting'] = observer.twilight_evening_astronomical(times, which='next').mjd
+    times = observer.twilight_evening_astronomical(times, which='next')
+    almanac['sun_n18_setting'] = times.mjd
     almanac['sun_n18_rising'] = observer.twilight_morning_astronomical(times, which='next').mjd
     almanac['sun_n12_rising'] = observer.twilight_morning_nautical(times, which='next').mjd
     almanac['sunrise'] = observer.sun_rise_time(times, which='next').mjd
-    almanac['moonset'] = observer.moon_set_time(times, which='next').mjd
-    print('moonrise')
-    almanac['moonrise'] = observer.moon_rise_time(times, which='next').mjd
     results.append(almanac)
+    mjd = almanac['sunrise'] + t_step
+
 
 almanac = np.concatenate(results)
-umjds, indx = np.unique(almanac['sunset'], return_index=True)
-almanac = almanac[indx]
 almanac['night'] = np.arange(almanac['night'].size)
 
 np.savez('sunsets.npz', almanac=almanac)
